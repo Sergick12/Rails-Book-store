e3a84ec6df4c7dc48bf7676f4582781370c65dbd
Sergick12 <cepgruddd1@gmail.com>
2022-11-10 05:01:06 +0300

    add_validates_genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,10 @@
 class Genre < ApplicationRecord
   has_many :books_genres
   has_many :books, through: :books_genres
+
+  validates_associated :books_genres
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 4 }
+  validates :name, uniqueness: true
 end
