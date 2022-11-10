f31503d96c17c44a6050169ae61afce9a40e0404
Sergick12 <cepgruddd1@gmail.com>
2022-11-10 04:54:51 +0300

    add_validates_author

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,10 @@
 class Author < ApplicationRecord
   has_many :authors_books
   has_many :books, through: :authors_books
+
+  validates_associated :authors_books
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 3 }
+
 end
