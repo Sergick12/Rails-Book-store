fea8abb90be931aa140f84dfb11fd36ac5023637
Sergick12 <cepgruddd1@gmail.com>
2022-11-10 04:59:43 +0300

    add_validates_book

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,15 @@ class Book < ApplicationRecord
   has_many :authors_books
   has_many :authors, through: :authors_books
   has_many :subscriptions
-  validates :name, length: { minimum: 3 }
-  validates :year, inclusion: { in: (1700..2022),
-                                message: '%<value> is not a valid size', allow_nil: true }
+
+  validates_associated :books_genres
+  validates_associated :authors_books
+  validates_associated :subscriptions
+
+  validates :name, presence: true , length: { minimum: 3 }
+  validates :year, numericality: { only_integer: true } , 
+  inclusion: { in: (1700..2022),
+              message: '%<value> is not a valid size', allow_nil: true }
+  validates :quantity, numericality: { only_integer: true }
+
 end
