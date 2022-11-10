bf5d01bc9147e9e1dbf46cc2f45da0cfaac81874
Sergick12 <cepgruddd1@gmail.com>
2022-11-10 05:11:29 +0300

    add_validates_books_genre

--- app/models/books_genre.rb
+++ app/models/books_genre.rb
@@ -3,4 +3,7 @@
 class BooksGenre < ApplicationRecord
   belongs_to :genre
   belongs_to :book
+
+  validates :book_id, presence: true, uniqueness: { scope: :genre_id }
+  validates :genre_id, presence: true
 end
