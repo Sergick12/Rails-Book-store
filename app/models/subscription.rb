6ac900211a1c0da647db382ba70bc1fb938e06ef
Sergick12 <cepgruddd1@gmail.com>
2022-11-10 05:01:34 +0300

    add_validates_subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,8 @@
 class Subscription < ApplicationRecord
   belongs_to :book
   belongs_to :subscriber
+
+  validates :is_active, inclusion: { in: [true, false]}
+  validates :start, presence: true
+  validates :finish, presence: true
 end
