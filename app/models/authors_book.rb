1255bf0547fbaddf9b18d65aed1035a829edd1e5
Sergick12 <cepgruddd1@gmail.com>
2022-11-10 05:10:55 +0300

    add_validates_authors_book

--- app/models/authors_book.rb
+++ app/models/authors_book.rb
@@ -3,4 +3,7 @@
 class AuthorsBook < ApplicationRecord
   belongs_to :author
   belongs_to :book
+
+  validates :author_id, presence: true, uniqueness: { scope: :book_id }
+  validates :book_id, presence: true
 end
