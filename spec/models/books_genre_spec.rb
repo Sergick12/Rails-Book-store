d109cd0606d0a6205c181754d2b820208f3087da
Sergick12 <cepgruddd1@gmail.com>
2022-11-15 00:22:24 +0300

    add_spec_folder

---
+++ spec/models/books_genre_spec.rb
@@ -0,0 +1,17 @@
+
+require 'rails_helper'
+
+RSpec.describe BooksGenre, type: :model do
+  describe 'associations' do 
+    it { is_expected.to belong_to(:book) }
+    it { is_expected.to belong_to(:genre) }
+  end
+
+  describe 'validations' do
+    subject {create(:books_genre)}
+
+    it { is_expected.to validate_presence_of(:book_id)}
+    it { is_expected.to validate_presence_of(:genre_id)}
+    it { is_expected.to validate_uniqueness_of(:book_id).scoped_to(:genre_id) }
+  end
+end
