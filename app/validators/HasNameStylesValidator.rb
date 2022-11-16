a5fb95be1f1d5f7936f9040b859234447a2914d0
Sergick12 <cepgruddd1@gmail.com>
2022-11-17 00:52:12 +0300

    fix_custom_validator_has_name_style

---
+++ app/validators/HasNameStylesValidator.rb
@@ -0,0 +1,12 @@
+class HasNameStylesValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if (value.nil? || value == "")
+      return record.errors.add(:name, "doesn't be empty") 
+     end
+     value.split.each { |x|
+       if x.match(/(^[A-Z])([a-z]+)/).nil?
+          record.errors.add(attribute ,:blank, message: "doesn't fit the style")
+       end
+     } 
+  end
+end
