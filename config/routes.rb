cfa9b073935ce8dc487014e17f3492cfd0abc424
Sergick12 <cepgruddd1@gmail.com>
2022-11-16 22:13:27 +0300

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :subscriptions
-  resources :authors_books
-  resources :subscribers
-  resources :authors
-  resources :books_genres
-  resources :books
-  resources :genres
+   resources :subscriptions
+  # resources :authors_books
+  # resources :subscribers
+   resources :authors
+  # resources :books_genres
+  # resources :books
+  # resources :genres
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
