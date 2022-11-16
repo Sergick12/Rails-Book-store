42c3ff85681865b2d83ad986826f92c7e094ee84
Sergick12 <cepgruddd1@gmail.com>
2022-11-16 21:37:03 +0300

    add_queries

---
+++ spec/queries/books/show_book_in_given_period_and_count_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Books::ShowBookInGivenPeriodAndCount do
+  before do
+    @subscriber = create(:subscriber)
+    create_list(:subscription, 5, start: DateTime.new(2018))
+  end
+
+  context '#call' do
+    it do
+      expect(described_class.call).to eq({2018 => 5})
+    end
+  end
+end
---
+++ spec/queries/books/show_uniq_all_book_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Books::ShowUniqAllBook do
+  before do
+    @subscriber = create(:subscriber)
+    create_list(:subscription, 8, subscriber: @subscriber)
+    create_list(:subscription, 2, :active_false)
+  end
+
+  context '#call' do
+    it do
+      expect(described_class.call).to eq(8)
+    end
+  end
+end
