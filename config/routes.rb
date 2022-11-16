e0e27ffa114eec09edb66f0aca484f8e01bd725b
Sergick12 <cepgruddd1@gmail.com>
2022-11-17 02:53:07 +0300

    np

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :authors do
+        resources :books
+      end
+    end
+  end
+
    resources :subscriptions
-  # resources :authors_books
-  # resources :subscribers
-   resources :authors
-  # resources :books_genres
-  # resources :books
-  # resources :genres
+   resources :authors_books
+   resources :subscribers
+   resources :books_genres
+   resources :books
+   resources :genres
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
