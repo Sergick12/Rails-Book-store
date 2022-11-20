83fa0722a09201a5b293643086157b18c511c87c
Sergick12 <cepgruddd1@gmail.com>
2022-11-21 00:21:20 +0300

    create_books_request_spec

--- spec/requests/authors_request_spec.rb
+++ spec/requests/authors_request_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Api/V1/Authors', type: :request do
@@ -6,25 +8,25 @@ RSpec.describe 'Api/V1/Authors', type: :request do
       create(:author_with_books, books_count: 2)
     end
 
-    it "creates a Authors" do
-      get "/api/v1/authors"
+    it 'creates a Authors' do
+      get '/api/v1/authors'
       expect(response).to have_http_status(:ok)
     end
 
-    it "retunrs five items" do
-      get "/api/v1/authors.json"
-      expect(JSON.parse(response.body).count).to eq(2)
+    it 'retunrs five items' do
+      get '/api/v1/authors.json'
+      expect(JSON.parse(response.body).count).to eq(1)
     end
   end
 
   describe '#show' do
-  before do
-    create(:author_with_books, books_count: 2)
-  end
+    before do
+      create(:author_with_books, books_count: 2)
+    end
 
-    it "returns count author books" do
-      get "/api/v1/authors/1"
-      expect(JSON.parse(response.body)["books"].count).to eq(2)
+    it 'returns count author books' do
+      get '/api/v1/authors/10'
+      expect(JSON.parse(response.body)['books'].count).to eq(2)
     end
   end
 end
---
+++ spec/requests/books_request_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api/V1/Books', type: :request do
+  describe '#index' do
+    before do
+      create(:author_with_books, books_count: 2)
+    end
+
+    it 'creates a Books' do
+      get '/api/v1/books'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'retunrs five items' do
+      get '/api/v1/books.json'
+      expect(JSON.parse(response.body).count).to eq(2)
+    end
+  end
+  describe '#show' do
+    before do
+        create(:book, :true_name)
+      end
+    it 'return book' do
+      get "/api/v1/books/#{Book.all.first.id}"
+      expect(JSON.parse(response.body)["name"]).to eq("name5")
+    end
+  end
+end
