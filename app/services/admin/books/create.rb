4a3a25d3f88e6b1506f52add12bc59ab99dfe071
Sergick12 <cepgruddd1@gmail.com>
2022-11-21 00:25:26 +0300

    create_books_admin

--- app/services/admin/books/create.rb
+++ app/services/admin/books/create.rb
@@ -1,43 +1,45 @@
+# frozen_string_literal: true
+
 module Admin
-    module Books
-      class Create < BaseService
-        def call
-          ActiveRecord::Base.transaction do
-            @book = Book.create!(book_params)
-            add_authors!
-            add_genres!
-            @book.save!
-          end
-          @book
+  module Books
+    class Create < BaseService
+      def call
+        ActiveRecord::Base.transaction do
+          @book = Book.create!(book_params)
+          add_authors!
+          add_genres!
+          @book.save!
         end
-  
-        private
-  
-        def add_authors!
-          authors_attributes.each do |author_attributes|
-            author = Author.find_or_create_by(author_attributes.permit(:id, :name))
-            @book.authors << author
-          end
+        @book
+      end
+
+      private
+
+      def add_authors!
+        authors_attributes.each do |author_attributes|
+          author = Author.find_or_create_by(author_attributes.permit(:id, :name))
+          @book.authors << author
         end
-  
-        def add_genres!
-          genres_attributes.each do |genre_attributes|
-            genre = Genre.find_or_create_by(genre_attributes.permit(:id, :name))
-            @book.genres << genre
-          end
+      end
+
+      def add_genres!
+        genres_attributes.each do |genre_attributes|
+          genre = Genre.find_or_create_by(genre_attributes.permit(:id, :name))
+          @book.genres << genre
         end
-  
-        def book_params
-          params.require(:book).permit(:name, :year, :quantity)
-        end
-  
-        def authors_attributes
-          @author_ids ||= (params[:book][:authors_attributes].presence || [])
-        end
-  
-        def genres_attributes
-          @genre_ids ||= (params[:book][:genres_attributes].presence || [])
-        end
+      end
+
+      def book_params
+        params.require(:book).permit(:name, :year, :quantity)
+      end
+
+      def authors_attributes
+        @author_ids ||= (params[:book][:authors_attributes].presence || [])
+      end
+
+      def genres_attributes
+        @genre_ids ||= (params[:book][:genres_attributes].presence || [])
       end
     end
   end
+end
