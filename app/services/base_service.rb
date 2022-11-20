45cdf97074eae9e01e58eb13def300a165b0a9d8
Sergick12 <cepgruddd1@gmail.com>
2022-11-21 00:23:57 +0300

    create_base_service

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class BaseService
-    attr_accessor :params
-  
-    def self.call(params = {})
-      new(params).call
-    end
-  
-    def initialize(params)
-      @params = params
-    end
-  
-    def call
-      raise NotImplementedError
-    end
+  attr_accessor :params
+
+  def self.call(params = {})
+    new(params).call
   end
+
+  def initialize(params)
+    @params = params
+  end
+
+  def call
+    raise NotImplementedError
+  end
+end
