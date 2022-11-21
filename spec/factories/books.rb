0f55731173af3f391572a3ceb149d0a1ddcd197f
Sergick12 <cepgruddd1@gmail.com>
2022-11-22 01:06:04 +0300

    add_trait_to_book_factorie

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     year { rand(2017..2020) }
     quantity { rand(1..15) }
     trait :true_name do
-      name { 'name5'}
+      name { 'name5' }
     end
     trait :old do
       year { rand(1900..1940) }
