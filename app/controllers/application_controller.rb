4a7879d72aeed37ed941ac9366d4522d393dbe6e
Sergick12 <cepgruddd1@gmail.com>
2022-11-22 01:04:31 +0300

    add_error_handler_422

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  #rescue_from Exception, with: :error_handler
-  # def error_handler(errors)
-  #   render json: errors, status: :unprocessable_entity
-  # end
+  rescue_from Exception, with: :error_handler
+  def error_handler(errors)
+    render json: errors, status: :unprocessable_entity
+  end
 end
