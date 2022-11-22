d35a229153c83a2e4bbce54384e8735e6b52ddd0
Sergick12 <cepgruddd1@gmail.com>
2022-11-22 06:43:07 +0300

    add_book_destroy

--- app/controllers/api/admin/books_controller.rb
+++ app/controllers/api/admin/books_controller.rb
@@ -37,11 +37,6 @@ module Api
       # DELETE /books/1 or /books/1.json
       def destroy
         @book.destroy
-
-        respond_to do |format|
-          format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-          format.json { head :no_content }
-        end
       end
 
       private
